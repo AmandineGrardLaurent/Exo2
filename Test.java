bb4285dfc9650937d51b971c2066db2841cda42d
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-10-27 15:36:23 +0100

    feat: Docstring and .gitignore

--- Test.java
+++ Test.java
@@ -2,29 +2,44 @@ import java.util.Scanner;
 
 public class Test {
 
+  /**
+   * Determines the parity of a number.
+   * @param number The number to check.
+   * @return "pair" if even, "impair" if odd.
+   */
     public static String parity(int number) {
         if(number % 2 == 0) return "pair";
         else return "impair";
     }
     
+    /**
+     * Determines the positivity of a number.
+     * @param number The number to check.
+     * @return "positif" if positive, "négatif" if negative, "nul" if zero.
+     */
     public static String positivity(int number) {
         if(number < 0) return "négatif";
         else if (number > 0) return "positif";
         else return "nul";
     }
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int number = 0;
 
+        // Check if a command-line argument was provided
         if (args.length > 0) number = Integer.parseInt(args[0]);
         else {
+            // Prompt user if no argument and read number from input
             System.out.println("Chiffre à tester : ");
             number = scan.nextInt();
         }
         
+        // Get the positivity and parity of the number
         String positivityResult = positivity(number);
         String parityResult = parity(number);
 
+        // Print the results
         System.out.println(number + " est " + positivityResult + " et " + parityResult);
     }
 }
